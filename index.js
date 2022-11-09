fe875745d900d65f8a506ffa0a0b1ba6bf0f9d46
Shafayet-Hossain23 <mdshafayethossain6530@gmail.com>
2022-11-09 23:05:39 +0600

    get-service-by-id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 app.use(cors())
@@ -30,6 +30,13 @@ async function run() {
             const count = await serviceCollection.estimatedDocumentCount();
             res.send({ count, services });
         });
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await serviceCollection.findOne(query);
+            res.send(result)
+            console.log(result)
+        })
     }
     finally {
 
